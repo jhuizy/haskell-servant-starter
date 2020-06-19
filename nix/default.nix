a22221ccb1b56b38e1741da1eb68fa7303f33d6f
jhuizy <jhuizenga99@gmail.com>
2020-06-19 16:39:38 +1000

    reran cabal2nix

--- nix/default.nix
+++ nix/default.nix
@@ -1,17 +1,18 @@
 { mkDerivation, aeson, base, http-types, mtl, persistent
-, persistent-sqlite, persistent-template, scotty, servant
+, persistent-sqlite, persistent-template, resource-pool, servant
 , servant-server, stdenv, text, warp
 }:
 mkDerivation {
   pname = "budget";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
-  executableHaskellDepends = [
+  libraryHaskellDepends = [
     aeson base http-types mtl persistent persistent-sqlite
-    persistent-template scotty servant servant-server text warp
+    persistent-template resource-pool servant servant-server text warp
   ];
+  executableHaskellDepends = [ base ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
